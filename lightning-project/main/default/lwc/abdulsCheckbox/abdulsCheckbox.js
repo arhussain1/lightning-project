f350b36add4660fe1fde770afb24cd18c94b203c
Abdul Hussain <a.r.hussain@hotmail.com>
2023-05-18 14:41:51 +0100

    add changes

--- lightning-project/main/default/lwc/abdulsCheckbox/abdulsCheckbox.js
+++ lightning-project/main/default/lwc/abdulsCheckbox/abdulsCheckbox.js
@@ -1,10 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import SKILLS_FIELD from '@salesforce/schema/OpportunityLineItem.Skills__c';
 
-export default class AbdulsCheckbox extends LightningElement {
-    handleChange(event) {
-        const isChecked = event.target.checked;
-        const checkboxName = event.target.name;
-    
-        console.log(`${checkboxName} is ${isChecked ? 'checked' : 'unchecked'}`);
-      }
+export default class OpportunityProductSkillsManager extends LightningElement {
+    @api recordId;
+    @track selectedSkills = [];
+
+    @wire(getRecord, { recordId: '$recordId', fields: [SKILLS_FIELD] })
+    opportunityProduct;
+
+    get skills() {
+        if (this.opportunityProduct.data && this.opportunityProduct.data.fields.Skills__c.value) {
+            return this.opportunityProduct.data.fields.Skills__c.value.split(', ');
+        }
+        return [];
+    }
+
+    get skillOptions() {
+        return this.skills.map(skill => ({ label: skill, value: skill }));
+    }
+
+    handleSkillsChange(event) {
+        this.selectedSkills = event.detail.value;
+    }
 }
